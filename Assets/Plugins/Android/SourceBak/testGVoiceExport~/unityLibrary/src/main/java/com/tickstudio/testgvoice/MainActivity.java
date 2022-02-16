7e65efbf6e3eee764927ff32440455e48b16869c
hengjin.xu <xiaoran301@gmail.com>
2022-02-16 10:49:08 +0800

    #testGVoice.jar# it's gradle project soucre bak

--- Assets/Plugins/Android/SourceBak/testGVoiceExport~/unityLibrary/src/main/java/com/tickstudio/testgvoice/MainActivity.java
+++ Assets/Plugins/Android/SourceBak/testGVoiceExport~/unityLibrary/src/main/java/com/tickstudio/testgvoice/MainActivity.java
@@ -4,7 +4,6 @@ package com.tickstudio.testgvoice;
 
 import android.os.Bundle;
 import com.gcloudsdk.gcloud.voice.GCloudVoiceEngine;
-import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends UnityPlayerActivity{
     protected void onCreate(Bundle savedInstanceState){
