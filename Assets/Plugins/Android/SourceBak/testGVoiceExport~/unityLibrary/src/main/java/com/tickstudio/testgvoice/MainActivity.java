5a3dbbb09e95cad311904b3b1dba75a89bcfc68d
hengjin.xu <xiaoran301@gmail.com>
2022-02-16 11:09:02 +0800

    #build apk success#

--- Assets/Plugins/Android/SourceBak/testGVoiceExport~/unityLibrary/src/main/java/com/tickstudio/testgvoice/MainActivity.java
+++ Assets/Plugins/Android/SourceBak/testGVoiceExport~/unityLibrary/src/main/java/com/tickstudio/testgvoice/MainActivity.java
@@ -3,11 +3,16 @@
 package com.tickstudio.testgvoice;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import com.gcloudsdk.gcloud.voice.GCloudVoiceEngine;
 
 public class MainActivity extends UnityPlayerActivity{
+    private static final String TAG = "MainActivity";
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        Log.v(TAG,"GCloudVoiceEngineBefore...");
         GCloudVoiceEngine.getInstance().init(getApplicationContext(),this);
+        Log.v(TAG,"GCloudVoiceEngineAfter...");
     }
 }
